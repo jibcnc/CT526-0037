ededce9bc8b2d4ee6778502125ee300167d09cd6
jibcnc <rivalzjib@gmail.com>
2025-09-23 08:37:13 +0700

    3

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import importlib
 from mylib import myfunc
 
-character = input("please input character: ") 
-rounds = int(input("How many turns you want to run: ")) 
+character = input("Input character: ") 
+rounds = int(input("Turns want to run: ")) 
 
 for i in range(1, rounds + 1):
     print(myfunc(character, i))
